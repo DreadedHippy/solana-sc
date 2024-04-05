134ae4a14ebf338f35348c5418c9fe0658de8d84
DreadedHippy <onotaizee@gmail.com>
2024-04-06 00:14:39 +0100

    Day 4 - Error handling and more

--- programs/solanasc/src/lib.rs
+++ programs/solanasc/src/lib.rs
@@ -1,16 +1,48 @@
 use anchor_lang::prelude::*;
 
-declare_id!("EWUPVRLV3vutGzRFR37Aj79rTARCGvmBYhja9KjNMJ1s");
+declare_id!("BP7EaKhfYcioQB6oVQDcSn5fbDTne5AoHYnk6gpnMyxU");
 
 #[program]
 pub mod solanasc {
+    use anchor_lang::solana_program::vote::error;
+
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>, a: u64) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, a: u64, message: String) -> Result<()> {
         msg!("You sent {}", a);
+        msg!("You said {}", message);
+        Ok(())
+    }
+
+    pub fn array(ctx: Context<Initialize>, arr: Vec<u64>) -> Result<()> {
+        msg!("Your array {:?}", arr);
+        Ok(())
+    }
+
+    pub fn limit_range(ctx: Context<Nextalize>, a: u64) -> Result<()> {
+        if a > 100 {
+            return err!(MyError::AisTooBig);
+        }
+
+        if a < 10 {
+            return err!(MyError::AisTooSmall);
+        }
+
+        msg!("Result = {}", a);
         Ok(())
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize {}
+
+#[derive(Accounts)]
+pub struct Nextalize {}
+
+#[error_code]
+pub enum MyError {
+    #[msg("a is too big")]
+    AisTooBig,
+    #[msg("a is too small")]
+    AisTooSmall,
+}
--- tests/solanasc.ts
+++ tests/solanasc.ts
@@ -1,5 +1,6 @@
 import * as anchor from "@coral-xyz/anchor";
-import { Program } from "@coral-xyz/anchor";
+import { AnchorError, Program } from "@coral-xyz/anchor";
+import { assert } from "chai";
 import { Solanasc } from "../target/types/solanasc";
 
 describe("solanasc", () => {
@@ -10,7 +11,43 @@ describe("solanasc", () => {
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize(new anchor.BN(777)).rpc();
+    const tx = await program.methods.initialize(new anchor.BN(777), "Hello :DDD").rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Array test", async () => {
+    const tx = await program.methods.array([new anchor.BN(777), new anchor.BN(888)]).rpc();
+    console.log("Your transaction signature is", tx)
+  })
+
+  it("Error test", async () => {
+    // Add your test here.
+
+    try {
+      const tx = await program.methods.limitRange(new anchor.BN(9)).rpc();
+      console.log("Your transaction signature", tx);
+    } catch (_err) {
+      assert.isTrue(_err instanceof AnchorError);
+      const err: AnchorError = _err;
+
+      const errMsg = "a is too small";
+
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log("Error number:", err.error.errorCode.number);
+    }
+
+    try {
+      const tx = await program.methods.limitRange(new anchor.BN(101)).rpc();
+      console.log("Your transaction signature", tx);
+    } catch (_err) {
+      assert.isTrue(_err instanceof AnchorError);
+      const err: AnchorError = _err;
+
+      const errMsg = "a is too big";
+
+      assert.strictEqual(err.error.errorMessage, errMsg);
+      console.log("Error number:", err.error.errorCode.number);
+    }
+    
+  })
 });
